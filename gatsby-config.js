60d93d86bedc90c9cc0e5e17e82bac652d8aad4d
michal <michalwols@gmail.com>
2020-04-12 20:47:43 -0400

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog MDX`,
-    author: `Matt Hagner`,
-    description: `An extension of the gatsby starter blog, with support for MDX`,
-    siteUrl: `https://gatsby-starter-blog-mdx-demo.netlify.com/`,
+    title: 'µ',
+    author: 'Michal Wolski',
+    description: 'A µ blog.',
+    siteUrl: 'michal.io',
     social: {
-      twitter: `mattinthecouch`,
+      twitter: `michalwols`,
     },
   },
   plugins: [
